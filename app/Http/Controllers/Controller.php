56f1f38fba0292e4aa13583cecc97407ac0fb70d
jdmejiav <jdmejiav@eafit.edu.co>
2021-10-19 16:02:28 -0500

    changes for docker sworm workshop

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,15 +24,11 @@ class Controller extends BaseController
     );
 
     public function index()
-
     {
-
         $totalQuotes = (count(Controller::$quotes));
-
-        $randomNumber = (rand(0, ($totalQuotes - 1)));
-
+        $randomNumber = (rand(0,($totalQuotes-1)));
         $randomQuote = Controller::$quotes[$randomNumber];
-
-        return response()->json(['quote' => $randomQuote]);
+        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
     }
+
 }
